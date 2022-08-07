e954e137b95da0a0630b1fa6e747fde603251ba3
DrGyrr <rozanwm@gmail.com>
2022-08-07 11:42:24 +0700

    menyelesaikan RGenerateFitInput.py, menambahkan folder venv dan pychache ke dalam gitignore agar ukuran repo tidak terlalu besar

--- RGenerateFitInput.py
+++ RGenerateFitInput.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 from ifrturbinepackage.rotor import *
 from ifrturbinepackage.definitions import *
 from tqdm import tqdm
+import glob
+import time
+import os
 
 # Gather Data for specific cycle, gparams set, and rpm
 
@@ -16,22 +19,23 @@ MaxVar1 = 14            #Max flow Coeffx10 in axis
 MinVar1 = 0.3             #Min flow Coeffx10 in axis
 MaxVar2 = 30            #Max Work Coeffx10 in axis
 MinVar2 = 2             #Min Work Coeffx10 in axis
-ndata1  = 10
-ndata2  = 10
+ndata1  = 15
+ndata2  = 15
 z_datapos = []
 x_datapos = []
 y_datapos = []
 dvar2   = (MaxVar2-MinVar2)/(ndata2-1)
 dvar1   = (MaxVar1-MinVar1)/(ndata1-1)
-dataindex=0
-countneg =0
-countValEr =0
+dataindex   = 0
+countneg    = 0
+countValEr  = 0
+countuse    = 0
 ndatatocalculate = ndata1*ndata2
 
-with tqdm (total=ndatatocalculate,desc='Computed Data',unit='pts',) as pbar:
+with tqdm (total=ndatatocalculate,desc='Computed',unit=' data') as pbar:
     for i in range(0,ndata2):
 
-
+            
             Var2        = MinVar2 + i*dvar2
             for j in range (0,ndata1):
                 try:
@@ -44,6 +48,7 @@ with tqdm (total=ndatatocalculate,desc='Computed Data',unit='pts',) as pbar:
                         z_datapos.append(ztoplot)
                         x_datapos.append(Var1)
                         y_datapos.append(Var2)
+                        countuse += 1
                     if ztoplot<=0:
                         countneg += 1
                     pbar.update(1)
@@ -51,24 +56,68 @@ with tqdm (total=ndatatocalculate,desc='Computed Data',unit='pts',) as pbar:
                     pbar.update(1)
                     countValEr += 1
                     continue
-                # pbar.set_postfix
-                # pbar.display(msg='\ntest', pos=None)
-                # print(f"\n Negative Data : {countneg}",end="\r")
-                # print(f"Value Error Data: {countValEr}",end="\r")
+
+                print(f"\n\x1b[2KNegative data amount: {countneg} ({countneg/ndatatocalculate*100:.1f}%)")
+                print(f"\x1b[2KValue Error data amount: {countValEr} ({countValEr/ndatatocalculate*100:.1f}%)")
+                print(f"\x1b[2KUseful data amount: {countuse} ({countuse/ndatatocalculate*100:.1f}%)",end="\r\033[A\033[A\033[A")
+                
                 
                 
 pbar.close()
-print(f"Negative data amount: {countneg}")
-print(f"Value Error data amount: {countValEr}")
-print("3D scatter plot is opened in new window")
 
-fig = plt.figure(figsize=(10,10))
+print("\n\n\n->> 3D scatter plot is opened in new window. After finished viewing it and set the angle to save, close to proceed ")
+
+fig = plt.figure(figsize=(12,12))
 ax  = plt.axes(projection = "3d")
 ax.scatter(x_datapos,y_datapos,z_datapos,lw=1,c= z_datapos,cmap="viridis")
 ax.set_title("Efficiency vs Flow Coeff and Work Coeff Fluida 1")
-ax.set_xlabel("Flow Coefficient")
-ax.set_ylabel("Work Coefficient")
-ax.set_zlabel("Efficiency (%)")
-plt.show()
+ax.set_xlabel("Flow Coefficient x10")
+ax.set_ylabel("Work Coefficient x10")
+ax.set_zlabel("Efficiency ,%")
 
-dosave = input("Would you like to save genrated data and the scatter plot? [Y/N]")
+
+
+saveddir  = os.path.join(ROOT_DIR,"Outputs","FitInputData")
+savedfigdir = os.path.join(saveddir,"Figures")
+fig.savefig(os.path.join(savedfigdir,'temp.png'),dpi=300,transparent=True)
+fig.savefig(os.path.join(savedfigdir,'temp.jpg'),dpi=300)
+plt.show()
+# print(ROOT_DIR,)
+
+
+while True:
+    dosave = str(input("Would you like to save generated data and the scatter plot? [Y/N] : "))
+    if dosave.upper() == 'Y' or dosave.upper() == 'YES':
+        # prevsaveddata = os.listdir(os.path.join(ROOT_DIR,"Outputs","FitInputData"))
+        # for filepath in fileslist:
+        #     timestamp   = time.strftime( '%m/%d/%Y :: %H:%M:%S', time.gmtime(os.path.getmtime(filepath)) )
+        #     print(timestamp,'-->',filepath)
+        print(f"Previously saved FitInput files:")
+        print(f"\nModified date & time{' '*8}File name")
+        print(f"{'-'*22}{' '*6}{'-'*15}")
+        listcontent(saveddir)
+        savenumber  = input("\nInsert save number to assign to generated data: ")
+        FitInputdict = {
+            '10xFlowCoeff'              : x_datapos,
+            '10xWorkCoeff'              : y_datapos,
+            'Total-Static_Efficiency'   : z_datapos
+        }
+        df = pd.DataFrame(FitInputdict)
+        datasavedas = os.path.join(saveddir,f"{savenumber}.csv")
+        plotsavedas = os.path.join(savedfigdir,f"{savenumber}.png")
+        df.to_csv(datasavedas)
+        fig.savefig(plotsavedas,dpi=300,transparent=True)
+        print(f"Generating FitInput Data done.")
+        print(f"Generated data saved as {savenumber}.csv")
+        print(f"Generated plot saved as {savenumber}.png and {savenumber}.jpg")
+        print("Plot in default position is also saved under name of temp.png and temp.jpg")
+        print(f"Now you can proceed to use generated data for curve fitting use")
+        # print("OK saved")
+        # print(dosave.upper())
+        break
+    elif dosave.upper() == 'N' or dosave.upper() == 'NO':
+        break
+    else :
+        print("Come on, wrong input. '_'")
+
+print("Program finished. You can close this now '_'")
--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,21 @@
-from tqdm import tqdm
-x=0
-for i in tqdm(range(10)):
-    x=x**(x**x)
-    print(i)
+import glob
+import os
+import time
+from ifrturbinepackage.definitions import *
+dir_name = ROOT_DIR
+
+dir_name = os.path.join(ROOT_DIR,"Outputs","FitInputData")
+# Get list of all files only in the given directory
+list_of_files = filter( lambda x: os.path.isfile(os.path.join(dir_name, x)),
+                        os.listdir(dir_name) )
+# Sort list of files based on last modification time in ascending order
+list_of_files = sorted( list_of_files,
+                        key = lambda x: os.path.getmtime(os.path.join(dir_name, x))
+                        )
+# Iterate over sorted list of files and print file path 
+# along with last modification time of file 
+for file_name in list_of_files:
+    file_path = os.path.join(dir_name, file_name)
+    timestamp_str = time.strftime(  '%m/%d/%Y || %H:%M:%S',
+                                time.gmtime(os.path.getmtime(file_path))) 
+    print(timestamp_str, ' -->', file_name)  
